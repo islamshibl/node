a019914df140b7c4615ea101bda071cdef102651
islamshibl <islam-shibl@hotmail.com>
2017-03-17 17:14:38 +0200

    Added NPM External module 'Reques'

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 const http = require('http');
 var os = require('os');
 var helper= require('./helpers/helperclass');
+var request =require('request');
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -17,7 +18,18 @@ const server = http.createServer((req, res) => {
   res.write('Host:' + os.hostname()+'\n');
   res.write("15 Min. load average: " + os.loadavg()[2]+'\n');
   res.write(helper.toMb(os.freemem())+' of '+ helper.toMb(os.totalmem())+' Mb free \n');
+ //using request "External npm module"
+ request('http://www.pluralsight.com/',function(error,response,body){
+   if(!error && response.statusCode===200){
+     //console.log(body);
   res.end('Hello World\n');
+
+   }
+
+ })
+ 
+ 
+ 
 
 });
 

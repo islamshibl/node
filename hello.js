b15ee6859a1db7b950d1579693fd130a1b3e905e
islamshibl <islam-shibl@hotmail.com>
2017-03-17 19:05:58 +0200

    Returning an Event Emitter from a function

--- hello.js
+++ hello.js
@@ -2,8 +2,27 @@ const http = require('http');
 var os = require('os');
 var helper = require('./helpers/helperclass');
 var request = require('request');
+var EventEmitter=require('events').EventEmitter;
 const hostname = '127.0.0.1';
 const port = 3000;
+
+
+var getResource =function(c){
+  var e =new EventEmitter();
+  process.nextTick(function(){
+    var count =0;
+    e.emit('start');
+    var t =setInterval(function(){
+      e.emit('data', ++count);
+        if(count ===c){
+          e.emit('end',count);
+          clearInterval(t);
+        }
+    },10);
+  });
+  return(e);
+}
+var resource =getResource(5);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -13,6 +32,8 @@ const server = http.createServer((req, res) => {
   res.write('Host:' + os.hostname() + '\n');
   res.write("15 Min. load average: " + os.loadavg()[2] + '\n');
   res.write(helper.toMb(os.freemem()) + ' of ' + helper.toMb(os.totalmem()) + ' Mb free \n');
+
+
   //using request "External npm module"
   request('http://www.pluralsight.com/', function (error, response, body) {
     if (!error && response.statusCode === 200) {
@@ -26,3 +47,15 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+  //Using EventEmitter
+  resource.on('start',function(){
+    console.log("I've Started!");
+  });
+
+  resource.on('data',function(data){
+    console.log("I've Received data  -> "+ data);
+  });
+
+  resource.on('end',function(t){
+    console.log("I'm done  with "+t +" data events");
+  });

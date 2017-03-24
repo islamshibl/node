f6308cebdf7cc1c3d7009e366ebac1351ce04eab
islamshibl <islam-shibl@hotmail.com>
2017-03-24 14:52:48 +0200

    Making web requests in Node

---
+++ client.js
@@ -0,0 +1,22 @@
+var http =require('http');
+
+var options ={
+    host :'www.pluralsight.com',
+    port :80,
+    path : '/',
+    method :'GET'
+};
+
+console.log('Going to make request ....');
+/*
+var req= http.request(options,function(response){
+    console.log(response.statusCode);
+    response.pipe(process.stdout);
+});
+req.end();*/
+
+
+http.get(options,function(response){
+    console.log(response.statusCode);
+    response.pipe(process.stdout);
+});
